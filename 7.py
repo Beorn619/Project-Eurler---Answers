b67e6b227f9a9944aa7d55be85c5e0be80a4d356
Beorn619 <qioime@yahoo.com>
2018-02-26 16:58:26 -0500

    modified 7.py

--- 7.py
+++ 7.py
@@ -1,1 +1,25 @@
+count = 0
+number = 0
 
+
+
+def isPrime(x):
+    i = 2
+    while i < x:
+        if x % i == 0:
+            return False
+        i = i + 1
+    return True
+
+
+
+while count < 10003:
+    if isPrime(number):
+        if count % 1000 == 0:
+            print(count)
+        count = count + 1
+    number = number + 1
+print(number - 1)
+
+
+        
