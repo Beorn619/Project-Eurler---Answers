ed89e1566a581aaf5492be6c28ccca2b7d62729a
Beorn619 <qioime@yahoo.com>
2018-03-10 12:21:13 -0500

    updated 10.py

--- 10.py
+++ 10.py
@@ -1,32 +1,31 @@
 import math
 
-x = 2000000
-y = 1415
-
-sieve = []
+goal = 100
+primes = [2]
 clean = []
+for n in range(3,goal,2):
+  sq = math.sqrt(n)
+  for prime in primes:
+    if sq < prime:
+      primes.append(n)
+      break
+    elif n%prime == 0:
+      break
+    else:
+      if primes[-1] != prime:
+        continue
+      else:
+        primes.append(n)
+        continue
+    primes.append(n)
 
-def generateNums():
-    for i in range(3, (x + 1), 2):
-        sieve.append(i)
-    sieve.insert(0, 2)
 
-
-def findPrimes():
-    for num in range(3, (y), 2):
-        for place in range(4, len(sieve), 1):
-            if sieve[place] % num == 0:
-                sieve[place] = 0
-    
 def cleanUp():
     total = 0
-    for i in range(0, len(sieve), 1):
-        if sieve[i] != 0:
-            clean.append(sieve[i])
-            total = total + sieve[i]
+    for i in range(0, len(primes), 1):
+        if primes[i] != 0:
+            clean.append(primes[i])
+            total = total + primes[i]
     print(total)
-
-
-generateNums()
-findPrimes()
+    
 cleanUp()
